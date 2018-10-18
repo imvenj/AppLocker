93c129c74cf01334b1f496a29c5b6a1cdf55faab
Venj <ersaclarke@gmail.com>
2018-10-18 16:08:03 +0800

    Move AppLocker to framework.

---
+++ PasscodeLock/AppLocker/AppLocker.h
@@ -0,0 +1,19 @@
+//
+//  AppLocker.h
+//  AppLocker
+//
+//  Created by venj on 10/18/18.
+//  Copyright © 2018 Oleg Ryasnoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppLocker.
+FOUNDATION_EXPORT double AppLockerVersionNumber;
+
+//! Project version string for AppLocker.
+FOUNDATION_EXPORT const unsigned char AppLockerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppLocker/PublicHeader.h>
+
+
--- PasscodeLock/PasscodeLock/ModeViewController.swift
+++ PasscodeLock/PasscodeLock/ModeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AppLocker
 
 class ModeViewController: UIViewController {
 
